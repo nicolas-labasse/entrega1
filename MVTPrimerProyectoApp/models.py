0a342b8204eeae9587f49e1362d4149ab3c64de3
nicolas-labasse <nicolaslabasse4@gmail.com>
2022-06-28 12:25:43 -0300

    push clases

--- MVTPrimerProyectoApp/models.py
+++ MVTPrimerProyectoApp/models.py
@@ -3,8 +3,20 @@ from django.db import models
 # Create your models here.
 
 
-class Persona(models.Model):
+class Materias(models.Model):
     nombre = models.CharField(max_length=50)
+    horas = models.IntegerField()
+
+
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
     edad = models.IntegerField()
-    nacimiento = models.DateField()
+    
+
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    materia = models.CharField(max_length=50)
   
